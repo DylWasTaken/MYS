a1e3969fdf05ac1a4ed32e37e2194eced5dc4465
Dylan M <Dylan.Mahowa@gmail.com>
2020-08-17 18:19:34 +0100

    server js spacing

--- server.js
+++ server.js
@@ -14,15 +14,16 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/log", require("./routes/api/log"));
 app.use("/api/profile", require("./routes/api/profile"));
 
-//Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  //Set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
-const PORT = process.env.Port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
